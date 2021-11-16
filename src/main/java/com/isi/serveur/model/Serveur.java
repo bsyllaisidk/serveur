fe403c034e7da77ee45e462ad0f96f26108f2310
Bakary SYLLA <bakarysylla58@gmail.com>
2021-11-16 20:31:48 +0000

    Initial commit

---
+++ src/main/java/com/isi/serveur/model/Serveur.java
@@ -0,0 +1,15 @@
+package com.isi.serveur.model;
+
+import com.isi.serveur.enumeration.Status;
+
+public class Serveur {
+    private int id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+
+
+}
